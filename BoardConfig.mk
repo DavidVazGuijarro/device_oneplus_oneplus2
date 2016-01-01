7f19dc6f86cb9a764be12e3005fd4f2573886084
bobzhome <BobZhome@cheerful.com>
2016-01-01 13:53:44 -0500

    Revert "Remove: Dexpreopt due to boot errors"
    This reverts commit dd2ee293e636be66219f35f9a2c1d6d29a5aa7a1.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -141,13 +141,13 @@ TARGET_NO_RPC := true
 WITH_DEXPREOPT := false
 
 # Enable dexpreopt to speed boot time
-#ifeq ($(HOST_OS),linux)
-#  ifeq ($(call match-word-in-list,$(TARGET_BUILD_VARIANT),user),true)
-#    ifeq ($(WITH_DEXPREOPT_BOOT_IMG_ONLY),)
-#      WITH_DEXPREOPT_BOOT_IMG_ONLY := true
-#    endif
-#  endif
-#endif
+ifeq ($(HOST_OS),linux)
+  ifeq ($(call match-word-in-list,$(TARGET_BUILD_VARIANT),user),true)
+    ifeq ($(WITH_DEXPREOPT_BOOT_IMG_ONLY),)
+      WITH_DEXPREOPT_BOOT_IMG_ONLY := true
+    endif
+  endif
+endif
 
 # Wifi
 BOARD_HAS_QCOM_WLAN := true
